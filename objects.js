6d89f7c8bbd268ce5a7e4f56f8d647f59e927793
cab driver <cocktailsthecomic@gmail.com>
2017-09-04 19:43:50 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,6 @@
 var playlist = new Object({artistName: 'songTitle'});
 function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = songTitle;
-  return playlist;
 }
 function removeFromPlaylist(playlist, artistName, songTitle){
   delete playlist.[artistName];
