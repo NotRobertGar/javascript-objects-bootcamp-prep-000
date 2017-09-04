ae246ec7ef33eff669587765bd10d50123eac840
cab driver <cocktailsthecomic@gmail.com>
2017-09-04 19:14:20 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = new Object();
+var playlist = new Object({"Taylor Swift": "Oatmeal"});
 
 function updatePlaylist(playlist, artistname, songtitle){
   return Object.assign({} playlist, {[artistname]: songtitle} );
